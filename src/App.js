20f68c01ff09dcc5debaf278910ad3063b1956c5
Doktorlich <sergei199522@mail.ru>
2022-08-08 17:55:32 +0300

    Create base early file
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    public/favicon.ico
    	deleted:    public/logo192.png
    	deleted:    public/logo512.png
    	modified:   src/App.js
    	renamed:    src/App.css -> src/App.scss
    	new file:   src/components/BodyPages/Body.jsx
    	new file:   src/components/BodyPages/Body.scss
    	new file:   src/components/Footer/Footer.jsx
    	new file:   src/components/Footer/Footer.scss
    	new file:   src/components/Header/Header.jsx
    	new file:   src/components/Header/Header.scss
    	new file:   src/components/Navigation/Navigation.jsx
    	new file:   src/components/Navigation/Navigation.scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Body from "./components/BodyPages/Body.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Header from "./components/Header/Header.jsx";
+import Navigation from "./components/Navigation/Navigation.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+           <Header />
+           <Navigation />
+           <Body />
+           <Footer />
+        </div>
+    );
+};
 
 export default App;

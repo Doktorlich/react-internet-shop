ea8b99bfacd9556ee3ea568072e40981d3d33de5
Doktorlich <sergei199522@mail.ru>
2022-08-11 21:58:40 +0300

    Add padding window
     Changes to be committed:
    	modified:   src/App.scss
    	modified:   src/components/BodyPages/MainHomePage/Main.scss
    	modified:   src/components/Footer/Footer.scss
    	modified:   src/components/Header/Header.jsx
    	modified:   src/components/Header/Header.scss
    	modified:   src/components/Navigation/Navigation.scss
    	modified:   src/globalStyle.scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,12 +17,28 @@ const Header = () => {
             </section>
             <hr />
             <section className="header__basement">
-                <div className="header__logo">
+                <div className="header_logo">
                     <h2>Freshnececom</h2>
                 </div>
                 <div className="header__form">
-                    <div className="header__form__filter"></div>
-                    <div className="header__form__search"></div>
+                    <div className="header__form_filter">
+                        <form name="form-filter">
+                            <select name="form-filter_list" id="">
+                                <option>All categories</option>
+                                <option value="Bakery">Bakery</option>
+                                <option value="Fruit and vegetables">Fruit and vegetables</option>
+                                <option value="Meat and fish">Meat and fish</option>
+                                <option value="Drinks">Drinks</option>
+                                <option value="Kitchen">Kitchen</option>
+                                <option value="Special nutrition">Special nutrition</option>
+                                <option value="Baby">Baby</option>
+                                <option value="Pharmacy">Pharmacy</option>
+                            </select>
+                        </form>
+                    </div>
+                    <div className="header__form__search">
+                        <input type="search" />
+                    </div>
                     <div className="header__form_icon-search"></div>
                 </div>
                 <div className="header__icons">

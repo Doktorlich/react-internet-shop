2fc7e498f67bd7308e9668795d578dfbba078c89
Doktorlich <sergei199522@mail.ru>
2022-08-21 19:28:58 +0300

    Testing Props-component in the BestSellingProducts
     Changes to be committed:
    	modified:   src/components/BodyPages/MainHomePage/BestSellingProducts/BestSellingProducts.jsx
    	modified:   src/components/BodyPages/MainHomePage/BestSellingProducts/ProductItem/ProductItem.jsx

--- src/components/BodyPages/MainHomePage/BestSellingProducts/BestSellingProducts.jsx
+++ src/components/BodyPages/MainHomePage/BestSellingProducts/BestSellingProducts.jsx
@@ -5,14 +5,17 @@ import MenuProduct from "./MenuProduct/MenuProduct";
 import ProductItem from "./ProductItem/ProductItem";
 
 const BestSellingProducts = () => {
+    const discountObject = {
+        discount: ["", "-36%", "-28%"],
+    };
     return (
         <section className="best-selling-products">
             <div className="best-selling-products__menu-product">
                 <MenuProduct />
                 <div className="best-selling-products__items">
-                <ProductItem/>
-                <ProductItem/>
-                <ProductItem/>
+                <ProductItem discount=""  price="1.48 USD"  priceDelete="" />
+                <ProductItem discount="-36%" price="0.28 USD"  priceDelete="48.56"/>
+                <ProductItem discount="-28%" price="1.12 USD"  priceDelete="48.56"/>
                 </div>
             </div>
         </section>
--- src/components/BodyPages/MainHomePage/BestSellingProducts/ProductItem/ProductItem.jsx
+++ src/components/BodyPages/MainHomePage/BestSellingProducts/ProductItem/ProductItem.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./ProductItem.scss";
 
 const ProductItem = (item) => {
+    
     return (
         <section className="product-item">
             <div className="product-item__items">
                 <div className="product-item__product-image">
                     <img src="" alt="" />
                     <span className="product-item__discount-icon">
-                        <span className="product-item__discount-number">-28%</span>
+                        <span className="product-item__discount-number">{item.discount}</span>
                     </span>
                 </div>
                 <div className="product-item__general">
@@ -18,8 +19,8 @@ const ProductItem = (item) => {
                     </div>
                     <div className="product-item__input">
                         <div className="product-item__text-form">
-                            <input type="text" className="product-item__text-form-1" value="22.6 USD" disabled />
-                            <input type="text" className="product-item__text-form-2" value="48.2" disabled />
+                            <input type="text" className="product-item__text-form-1" value={item.price} disabled />
+                            <input type="text" className="product-item__text-form-2" value={item.priceDelete} disabled />
                         </div>
 
                         <input type="button" value="Buy now" className="product-item__button-buy" />
